c2c471509dc1d616a36d3591b7f49057b556b969
Chad Saglam <saglam.chd@gmail.com>
2019-10-08 18:05:17 +0200

    Design a new custom attribute directive and apply it to elements within a template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -40,6 +40,7 @@ import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { LoginComponent } from './login/login.component';
+import { HighlightDirective } from './directives/highlight.directive';
 
 @NgModule({
   declarations: [
@@ -51,7 +52,8 @@ import { LoginComponent } from './login/login.component';
     AboutComponent,
     HomeComponent,
     ContactComponent,
-    LoginComponent
+    LoginComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
